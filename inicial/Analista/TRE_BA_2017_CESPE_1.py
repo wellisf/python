2f2db1a41b9b57af22cd42bbe0610e6dc275fb79
sswellington <wellingtondessilva@gmail.com>
2019-01-12 15:56:33 -0200

    feat: Add questions of TRE

---
+++ inicial/Analista/TRE_BA_2017_CESPE_1.py
@@ -0,0 +1,21 @@
+'''
+(CESPE – 2017 – TRE/BA - Analista de Sistemas)
+
+Assinale a opção que apresenta a saída resultante da execução do algoritmo
+antecedente.
+a)12345
+b)1245
+c)3
+d)0124
+e)1234
+'''
+
+i = 0
+while i < 5 :
+
+	i += 1
+
+	if (i == 3) :
+		continue
+
+	print(i)	
