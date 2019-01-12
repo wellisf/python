9605b2d39f722200f5ca0acbde0583a29576d247
Wellington Silva <45939490+sswellington@users.noreply.github.com>
2019-01-12 15:18:40 -0200

    Create question_2011_TRE.py

---
+++ inicial/question_2011_TRE.py
@@ -0,0 +1,15 @@
+'''  
+(IADES - 2011 – TRE- Programador de Computador)
+'''
+
+n1 = 2
+n2 = 30
+
+while n1 < n2:
+  n2 += n1
+  n1 *= 3
+
+n1 += 11
+
+print('N1 is equal: ',n1)
+print('N2 is equal: ',n2)
